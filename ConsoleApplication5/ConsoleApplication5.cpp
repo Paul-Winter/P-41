b99e80dd2aa9704ebf791675cc6d3c8859151474
Paul Winter <zimin.pavel.v@gmail.com>
2025-09-11 18:52:51 +0300

    практика со строками

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	char i = 0;
+	cout << "Введите символ ";
+	cin >> i;
+	/*for (int j = 0; j < 200; j++)
+	{
+		cout << j << " " << char(j) << endl;
+	}*/
+	if (int(i) >= 48 && int(i) <= 57)
+	{
+		cout << "Цифра"<<endl;
+	}
+	else if((int(i)>=65 && int(i)<=90)||(int(i)>=97 &&int(i)<=122))
+	{
+		cout << "Буква"<<endl;
+	}
+	else
+	{
+		cout << "Спец символ"<<endl;
+	}
+}
+
+
