270456cee799f6fbe3adc8a51f1840c4d4299b8a
Paul Winter <zimin.pavel.v@gmail.com>
2025-09-16 17:17:19 +0300

    classwork

---
+++ FirstLesson/Doge.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLesson
+{
+    internal class Doge
+    {
+        string name;
+        int age;
+        string colour;
+        string breed;
+        bool isAlive;
+
+        public Doge(string name, int age, string colour, string breed)
+        {
+            this.name = name;
+            this.age = age;
+            this.colour = colour;
+            this.breed = breed;
+            this.isAlive = true;
+        }
+
+        public string sayBreed()
+        {
+            return breed;
+        }
+
+        public void IsAlive()
+        {
+            if (isAlive)
+            {
+                Console.WriteLine("Bark Bark");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+        }
+
+        public void Killer()
+        {
+            isAlive = false;
+            Console.WriteLine($"RIP {name}");
+        }
+    }
+}
---
+++ FirstLesson/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto car = new Auto(2, "Green", "Lada", 110);
+            
+            
+            Console.WriteLine(car.calculator(10));
+        }
+    }
+}
