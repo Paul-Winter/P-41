a50d7258d5d37f65459d9c60ef9fcd841f20410f
Paul Winter <zimin.pavel.v@gmail.com>
2025-09-16 18:34:33 +0300

    multi arrays classwork

---
+++ Урок №15. Многомерные массивы/Урок №15. Многомерные массивы.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    //// количество строк
+    //int rows = 5;
+    //// количество столбцов
+    //int cols = 5;
+
+    int rows, cols;
+    cout << "Сколько строк? ";
+    cin >> rows;
+
+    // двумерный динамический массив
+    int** pArr = new int* [rows];
+    for (int i = 0; i < rows; i++)
+    {
+        cout << "Сколько столбцов? ";
+        cin >> cols;
+        pArr[i] = new int[cols];
+
+        for (int j = 0; j < cols; j++)
+        {
+            cout << "Введите оценку: ";
+            cin >> pArr[i][j];
+        }
+        cout << endl;
+    }
+
+    //pArr[2][2] = 1234;
+    //cout << pArr[2][2] << endl;
+
+    // удаление двумерного массива
+    for (int i = 0; i < rows; i++)
+    {
+        delete[] pArr[i];
+    }
+    delete[] pArr;
+
+    return 0;
+}
