a2f2144ed530f9c827ee7700a731339b7362336e
Paul Winter <zimin.pavel.v@gmail.com>
2025-09-23 19:09:05 +0300

    classwork

--- Урок №15. Перечислимые типы/Урок №15. Перечислимые типы.cpp
+++ Урок №15. Перечислимые типы/Урок №15. Перечислимые типы.cpp
@@ -11,31 +11,42 @@ enum Seasons
     AUTUMN
 };
 
+enum Menu { SIGN_IN = 1, REGISTER = 2, QUIT = 3 };
+
+// Бочаров      -   дни недели
+// Газарян      -   месяцы
+// Дымочкина    -   города
+// Лобозев      -   планеты солнечной системы
+// Середа       -   праздники
+// Сухота       -   дни недели
+// Умханов      -   месяцы
+
 int main()
 {
     setlocale(LC_ALL, "");
 
-    int season;
-    cout << "Введите время года: ";
-    cin >> season;
-    cout << endl;
+    int userChoise;
+    do
+    {
+        cout << "Выберите действие:" << endl;
+        cout << "1 - войти" << endl;
+        cout << "2 - зарегистрироваться" << endl;
+        cout << "3 - выход" << endl;
+        cin >> userChoise;
 
-    if (season == 4) 
-    {
-        cout << "Осень - золотая пора!" << endl;
-    }
-    else if (season == 1)
-    {
-        cout << "Зима - пора снежная." << endl;
-    }
-    else if (season == 2)
-    {
-        cout << "Весна красна пришла!" << endl;
-    }
-    else
-    {
-        cout << "Лето, ах лето!" << endl;
-    }
+        switch (userChoise)
+        {
+        case SIGN_IN: cout << "Введите логин и пароль!" << endl;
+            break;
+        case REGISTER: cout << "Введите логин, пароль и подтверждение пароля!" << endl;
+            break;
+        case QUIT: cout << "До свидания!" << endl;
+            break;
+        default: cout << "ERROR! Неправильно выбран номер пункта меню!" << endl;
+            break;
+        }
+        cout << endl;
+    } while (userChoise != 3);
 
     return 0;
 }
