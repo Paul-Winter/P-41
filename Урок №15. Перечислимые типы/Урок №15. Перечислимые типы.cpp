46657e26f282021e9f7dc295781730a35995233e
Paul Winter <zimin.pavel.v@gmail.com>
2025-09-23 17:39:53 +0300

    enum seasons

---
+++ Урок №15. Перечислимые типы/Урок №15. Перечислимые типы.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+enum Seasons
+{
+    WINTER,
+    SPRING,
+    SUMMER,
+    AUTUMN
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int season;
+    cout << "Введите время года: ";
+    cin >> season;
+    cout << endl;
+
+    if (season == 4) 
+    {
+        cout << "Осень - золотая пора!" << endl;
+    }
+    else if (season == 1)
+    {
+        cout << "Зима - пора снежная." << endl;
+    }
+    else if (season == 2)
+    {
+        cout << "Весна красна пришла!" << endl;
+    }
+    else
+    {
+        cout << "Лето, ах лето!" << endl;
+    }
+
+    return 0;
+}
