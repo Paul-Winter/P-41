773e6cd94f51fb6d06a556ae7925713e45ddd547
Paul Winter <zimin.pavel.v@gmail.com>
2025-09-25 17:44:39 +0300

    добавлены структуры и не только

---
+++ Урок №16. Структуры/Урок №16. Структуры.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct grandFather
+{
+    int age;
+    string name;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int age = 69;
+    cout << "age = " << age << endl;
+    string name = "Иван Иваныч";
+    cout << "name = " << name << endl;
+    
+    grandFather grandPa;
+    grandPa.name = "Пётр Петрович";
+    grandPa.age = 68;
+
+    cout << "name: " << grandPa.name << endl << "age: " << grandPa.age << endl;
+
+    return 0;
+}
