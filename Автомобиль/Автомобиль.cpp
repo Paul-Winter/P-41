c920b33477fc6bd764f66deba22c8eb93b6c68e2
Paul Winter <zimin.pavel.v@gmail.com>
2025-09-25 19:53:18 +0300

    car

---
+++ Автомобиль/Автомобиль.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+
+struct car
+{
+    double longcar;
+    double clirens;
+    double VEngine;
+    int P;
+    double d;
+    string color;
+    string box;
+};
+
+car carselect(double longc,double clir,double bEng,int PC,double dC, string color, string box)
+{
+    car machine = { longc,clir,bEng,PC,dC,color,box };
+    return machine;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+}
