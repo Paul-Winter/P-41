41237ec5e5342bdd1db2b0c085f628858746e06b
Paul Winter <zimin.pavel.v@gmail.com>
2025-09-30 19:55:15 +0300

    binary math operations

--- Урок №17. Двоичная арифметика/Урок №17. Двоичная арифметика.cpp
+++ Урок №17. Двоичная арифметика/Урок №17. Двоичная арифметика.cpp
@@ -6,7 +6,42 @@ int main()
 {
     setlocale(LC_ALL, "");
 
-    cout << "Привет, Мир!" << endl;
+    unsigned int x = 4;
+    cout << "до сдвига влево: x = " << x << endl;
+    x = x << 1;
+    cout << "после сдвига влево: x = " << x << endl;
+    cout << endl << endl;
+    cout << "_____________________________________________________________" << endl;
+
+    unsigned int a = 12;
+    for (int i = 0; i < 4; i++)
+    {
+        unsigned int b = a >> i;
+        cout << "a >> " << i << " = " << b << endl;
+    }
+    cout << endl << endl;
+    cout << "_____________________________________________________________" << endl;
+
+    int k, l, m, n;
+    
+    // побитовое ИЛИ
+    k = 5;
+    l = 6;
+    m = 7;
+    n = k | l;
+    cout << "5 | 6 = " << n << endl;
+
+    // побитовое И
+    n = k & l;
+    cout << "5 & 6 = " << n << endl;
+
+    // побитовое XOR
+    n = k ^ l;
+    cout << "5 ^ 6 = " << n << endl;
+
+    // побитовое НЕ
+    n = ~m;
+    cout << "~7 = " << n << endl;
 
     return 0;
 }
