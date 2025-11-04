11d97be6a7b56631f459727f9dca6ea3d6639595
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-04 18:51:54 +0300

    инкапсуляция, конструкторы и деструктор

--- ООП/Урок №1. Введение в ООП/Student.h
+++ ООП/Урок №1. Введение в ООП/Student.h
@@ -1,11 +1,68 @@
 #pragma once
+#include <iostream>
+
 class Student
 {
-public:
+private:
     // ФИО
     char name[30];
     // оценки
     int marks[5];
+
+public:
+    // конструктор по умолчанию
+    Student()
+    {
+        setName("Иван Безымянный");
+        for (int i = 0; i < 5; i++)
+        {
+            marks[i] = 1;
+        }
+    }
+    // конструктор параметризованный
+    Student(const char* studentName)
+    {
+        setName(studentName);
+        for (int i = 0; i < 5; i++)
+        {
+            marks[i] = 1;
+        }
+    }
+    //~Student()
+    //{
+    //    if (name != nullptr)
+    //    {
+    //        delete[] name;
+    //    }
+    //    if (marks != nullptr)
+    //    {
+    //        delete[] marks;
+    //    }
+    //    std::cout << "сработал деструктор" << std::endl;
+    //}
+    // get-функция, аксессор, инспектор
+    const char* getName()
+    {
+        return name;
+    }
+    // set-функция, мутатор
+    void setName(const char* studentName)
+    {
+        strcpy_s(name, 30, studentName);
+    }
+    int getMark(int index)
+    {
+        return marks[index];
+    }
+    void setMark(int mark, int index)
+    {
+        if (mark < 1 || mark > 12)
+        {
+            mark = 1;
+        }
+        marks[index] = mark;
+    }
+
     // средний балл
     double getAver();
     // показать все оценки
--- ООП/Урок №1. Введение в ООП/Урок №1. Введение в ООП.cpp
+++ ООП/Урок №1. Введение в ООП/Урок №1. Введение в ООП.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 struct Stud
 {
+public:
     // ФИО
     char name[30];
     // оценки
@@ -37,29 +38,73 @@ int main()
     setlocale(LC_ALL, "");
 
     //Stud studStruct;
-    Student studClass;
+    //Student studClass;
 
-    strcpy_s(studClass.name, 20, "Иванов И.И.");
-    //strcpy_s(studStruct.name, 20, "Петров П.П.");
-    
-    //studStruct.marks[0] = 1;
-    studClass.marks[0] = 11;
-    studClass.marks[1] = 10;
-    studClass.marks[2] = 12;
-    studClass.marks[3] = 9;
-    studClass.marks[4] = 9;
+    //studClass.setName("Иванов П.П.");
 
-    //cout << "Студент структуры: " << studStruct.name << " оценка: " << studStruct.marks[0] << endl;
-    //cout << "Студент класса: " << studClass.name << " оценка: " << studClass.marks[0] << endl;
+    ////strcpy_s(studClass.name, 20, "Иванов И.И.");
+    ////strcpy_s(studStruct.name, 20, "Петров П.П.");
+    //
+    ////studStruct.marks[0] = 1;
+    ////studClass.marks[0] = 11;
+    ////studClass.marks[1] = 10;
+    ////studClass.marks[2] = 12;
+    ////studClass.marks[3] = 9;
+    ////studClass.marks[4] = 9;
 
-    //AccessLevels al;
-    //al.Print();
-    //al.publicMember = 13;
-    //al.Print();
+    //studClass.setMark(11, 0);
+    //studClass.setMark(10, 1);
+    //studClass.setMark(12, 2);
+    //studClass.setMark(9, 3);
+    //studClass.setMark(9, 4);
 
-    cout << "Студент: " << studClass.name << endl;
-    studClass.allMarks();
-    cout << "Средний балл: " << studClass.getAver() << endl;
+    ////cout << "Студент структуры: " << studStruct.name << " оценка: " << studStruct.marks[0] << endl;
+    ////cout << "Студент класса: " << studClass.name << " оценка: " << studClass.marks[0] << endl;
+
+    ////AccessLevels al;
+    ////al.Print();
+    ////al.publicMember = 13;
+    ////al.Print();
+
+    //cout << "Студент: " << studClass.getName() << endl;
+    //studClass.allMarks();
+    //cout << "Средний балл: " << studClass.getAver() << endl;
+
+    //cout << endl << "______________________________________________" << endl << endl;
+
+    //Student stud;
+    //stud.setName("Петров И.И.");
+
+    ////strcpy_s(stud.name, 20, "sdfagaregre");
+    ////stud.marks[0] = 12;
+    ////stud.marks[1] = 12;
+    ////stud.marks[2] = 12;
+    ////stud.marks[3] = 12;
+    ////stud.marks[4] = 12;
+    //stud.setMark(9, 0);
+    //stud.setMark(10, 1);
+    //stud.setMark(10, 2);
+    //stud.setMark(11, 3);
+    //stud.setMark(9, 4);
+
+    //cout << "Студент: " << stud.getName() << endl;
+    //stud.allMarks();
+    //cout << "Средний балл: " << stud.getAver() << endl;
+
+    cout << endl << "______________________________________________" << endl << endl;
+
+    Student stud;
+    cout << stud.getName() << endl;
+    stud.allMarks();
+    cout << "Средний балл: " << stud.getAver() << endl;       
+
+
+    //cout << endl << "______________________________________________" << endl << endl;
+
+    //Student stud3("Сидоров А.Б.");
+    //cout << stud3.getName() << endl;
+    //stud3.allMarks();
+    //cout << "Средний балл: " << stud3.getAver() << endl;
 
     return 0;
 }
