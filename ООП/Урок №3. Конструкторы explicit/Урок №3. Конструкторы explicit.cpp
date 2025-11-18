ca8accc1dcc5682d596a68f1dfcb55dd073511ea
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-18 19:13:36 +0300

    неявные преобразования типов

---
+++ ООП/Урок №3. Конструкторы explicit/Урок №3. Конструкторы explicit.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Date
+{
+private:
+    int day;
+    int month;
+    int year;
+public:
+    Date(int day, int month, int year) : day{day}, month{month}, year{year}
+    {}
+    Date(int year) : Date(1, 1, year)
+    {}
+    int getDay() { return day; }
+    int getMonth() { return month; }
+    int getYear() { return year; }
+};
+
+void Print(Date date)
+{
+    cout << date.getDay() << "." << date.getMonth() << "." << date.getYear() << endl;
+}
+
+Date currentYear()
+{
+    return 2025;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    Print(2020);
+    Date date = 2010;
+    Print(date);
+    Date current = currentYear();
+    Print(current);
+
+    return 0;
+}
