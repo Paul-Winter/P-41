6dccb1b0cd9fd73d28d7c6d67061457cb500dee2
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-18 18:42:23 +0300

    константные методы практика

--- ООП/Урок №3. Константные методы/Урок №3. Константные методы.cpp
+++ ООП/Урок №3. Константные методы/Урок №3. Константные методы.cpp
@@ -55,20 +55,58 @@ public:
     }
 };
 
+class Account
+{
+private:
+    double sum;
+    const double rate;
+public:
+    Account(double sum, double rate) : sum{ sum }, rate{ rate } {}
+    double getSum()
+    {
+        return sum;
+    }
+    double getSum() const
+    {
+        return sum;
+    }
+
+    double getRate()
+    {
+        return rate;
+    }
+    double getRate() const
+    {
+        return rate;
+    }
+    
+    void setSum(double value)
+    {
+        sum = value;
+    }
+};
+
 int main()
 {
     setlocale(LC_ALL, "");
 
-    const Date today{ 18,11,2025 };
-    Date yesterday{ 18,11,2025 };
+    Account account1{ 110.21, 4.5 };
+    const Account account2{ 4012.94, 4.9 };
 
-    cout << "Сегодня " << today.getDay() << "-е число" << endl;
-    cout << "Вчера было " << yesterday.getDay() << "-е число" << endl;
 
-    yesterday.setDay(17);
-    cout << "Вчера было " << yesterday.getDay() << "." << yesterday.getMonth() << "." << yesterday.getYear() << endl;
-
-    cout << "Сейчас идёт " << today.getCurrYear() << " год" << endl;
+    cout << "Здраствуйте! Спасибо что запустили банк Opat\nВаш баланс: " << account1.getSum() << endl << "Ваш рейтинг в нашем банке: " << account1.getRate() << endl;
+    cout << endl << endl << endl;
+    cout << "Здраствуйте! Спасибо что запустили банк Opat\nВаш баланс: " << account2.getSum() << endl << "Ваш рейтинг в нашем банке: " << account2.getRate() << endl;
+    //const Date today{ 18,11,2025 };
+    //Date yesterday{ 18,11,2025 };
+    //
+    //cout << "Сегодня " << today.getDay() << "-е число" << endl;
+    //cout << "Вчера было " << yesterday.getDay() << "-е число" << endl;
+    //
+    //yesterday.setDay(17);
+    //cout << "Вчера было " << yesterday.getDay() << "." << yesterday.getMonth() << "." << yesterday.getYear() << endl;
+    //
+    //cout << "Сейчас идёт " << today.getCurrYear() << " год" << endl;
 
     return 0;
 }
