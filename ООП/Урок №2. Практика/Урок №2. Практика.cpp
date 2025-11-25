91ee80546b396c4625ab136583aa92d82f39d44a
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-25 19:53:35 +0300

    класс дробь TODO: доделать операторы

--- ООП/Урок №2. Практика/Урок №2. Практика.cpp
+++ ООП/Урок №2. Практика/Урок №2. Практика.cpp
@@ -10,8 +10,12 @@ using namespace std;
 // + конструктор принимающий числитель и знаменатель
 // + конструктор по умолчанию
 // + вывод дроби на экран
-// - сложение, вычитание, умножение простой дроби с простой дробью
-// - сложение, вычитание, умножение простой дроби с целым числом
+// + сложение
+// - вычитание
+// - умножение простой дроби с простой дробью
+// - сложение
+// - вычитание
+// + умножение простой дроби с целым числом
 // - В арифметических операциях предусмотреть вызов операций по цепочке (this)
 // + Предусмотреть сокращение дроби (конструктор)
 
@@ -19,26 +23,26 @@ class Fraction
 {
     int numerator;
     int denominator;
-public:
 
+public:
     Fraction(int num, int denom) 
     {
         int nod = Nod(num, denom);
         numerator = num / nod;
         denominator = denom / nod;
-        cout<< "numerator = " << numerator << endl << "deniminator = " << denominator << endl;
+        this->Print();
     }
     Fraction() : Fraction(1,1) {}
     void Print()
     {
         cout << "(" << numerator << '/' << denominator << ")" << endl;
     }
-    Fraction Mult(Fraction f1, int num)
+    Fraction Mult(Fraction f, int num)
     {
-        f1.numerator *= num;
-        f1.denominator *= num;
-        f1.Print();
-        return f1;
+        f.numerator *= num;
+        f.denominator *= num;
+        f.Print();
+        return f;
     }
     Fraction& Sum(Fraction f1, Fraction f2)
     {
@@ -46,8 +50,29 @@ public:
         f2.Mult(f2, f1.denominator);
         numerator = f1.numerator + f2.numerator;
         denominator = f1.denominator;
-        cout << "numerator = " << numerator << endl << "deniminator = " << denominator << endl;
+        cout << "numerator = " << numerator << endl << "denominator = " << denominator << endl;
         return *this;
+    }
+    friend Fraction& operator*(Fraction& f, int num)
+    {
+        f.numerator *= num;
+        f.denominator *= num;
+        f.Print();
+        return f;
+    }
+    friend Fraction& operator+(Fraction& f1, Fraction& f2)
+    {  
+        Fraction result{1,1};
+        //f1.Mult(f1, f2.denominator);
+        //f2.Mult(f2, f1.denominator);
+        f1 = f1 * f2.denominator;
+        f2 = f2 * f1.denominator;
+        //numerator = f1.numerator + f2.numerator;
+        //denominator = f1.denominator;
+        result.numerator = f1.numerator + f2.numerator;
+        result.denominator = f1.denominator;
+        //cout << "numerator = " << numerator << endl << "denominator = " << denominator << endl;
+        return result;
     }
 private:   
     int Nod(int numerator, int denominator)
@@ -70,14 +95,17 @@ int main()
 {
     setlocale(LC_ALL, "");
 
-    Fraction  frac{1,4};
+    Fraction frac{ 1,4 };
     frac.Print();
     
     Fraction test{ 1,6 };
     test.Print();
    
-    frac.Sum( frac,test );
+    //frac = frac.Sum(frac, test);
     frac.Print();
+    Fraction res{};
+    res = frac + test;
+    res.Print();
 
     return 0;
 }
