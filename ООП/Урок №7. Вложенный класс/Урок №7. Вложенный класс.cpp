f7a24d3c02400800c6860dd9cfaff9d31203b47e
Paul Winter <zimin.pavel.v@gmail.com>
2025-12-18 18:39:44 +0300

    вложенные классы

---
+++ ООП/Урок №7. Вложенный класс/Урок №7. Вложенный класс.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Human
+{
+public:
+    string name;
+    int age;
+//private:
+    // inner class
+    class Organ
+    {
+    public:
+        string name;
+        int weight;
+        Organ(int _weight) : weight{ _weight } {}
+        void PrintOrgan()
+        {
+            cout << "Organ weight is " << weight << " gramm" << endl;
+        }
+    };
+public:
+    Human(int _age) : age{ _age } {}
+    Human(string _name) : name{_name} {}
+    void PrintHuman()
+    {
+        cout << "Human name is " << name << " age: " << age << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    
+    Human human("Ivan");
+    human.age = 17;
+    human.PrintHuman();
+
+    Human::Organ appendex(200);
+    appendex.PrintOrgan();
+    
+    return 0;
+}
