003fa8ec3bb195b51eb0d25eaf58a3a678a3a69c
Paul Winter <zimin.pavel.v@gmail.com>
2026-01-15 19:25:18 +0300

    полиморфизм при множественном наследовании

--- ООП/Урок №8. Абстрактные классы/Урок №8. Абстрактные классы.cpp
+++ ООП/Урок №8. Абстрактные классы/Урок №8. Абстрактные классы.cpp
@@ -30,7 +30,6 @@ public:
         cout << "Имя: " << name << endl;
     }
 };
-
 class Dog :public Pet
 {
 protected:
@@ -84,7 +83,6 @@ protected:
     virtual void Show() = 0;
     virtual void Say() = 0;
 };
-
 class Frog: public Animal
 {
     string color;
@@ -118,6 +116,167 @@ public:
     virtual void Say() { cout << "Shhhh" << endl; }
 };
 
+class Human
+{
+protected:
+    string name;
+    int age;
+    int height;
+    int weight;
+    char gender;
+
+public:
+    Human(string name, int age, int height, int weight, char gender) : name{ name }, age{ age }, height{ height }, weight{ weight }, gender{ gender } {}
+    Human() {}
+    //virtual void Show() = 0;
+    virtual void Show()
+    {
+        cout << name << "\t возраст: " << age << "\t рост:" << height << "\t вес: " << weight << endl;
+    }
+    virtual string GetName()
+    {
+        return name;
+    }
+    virtual int GetAge()
+    {
+        return age;
+    }
+    virtual int GetHeight()
+    {
+        return height;
+    }
+    virtual int GetWeight()
+    {
+        return weight;
+    }
+    virtual int GetGender()
+    {
+        return gender;
+    }
+    virtual void SetName(string value)
+    {
+        name = value;
+    }
+    virtual void SetAge(int value)
+    {
+        age = value;
+    }
+    virtual void SetWeight(int value)
+    {
+        weight = value;
+    }
+    virtual void SetHeight(int value)
+    {
+        height = value;
+    }
+};
+class Mother : public Human
+{
+protected:
+    string eyes;
+
+public:
+    Mother(string name, int age, int height, int weight, string eyes) : eyes{ eyes } 
+    {
+        this->name = name;
+        this->age = age;
+        this->height = height;
+        this->weight = weight;
+        this->gender = 'Ж';
+        this->eyes = eyes;
+    }
+    Mother() : Mother("Мамаб", 62, 160, 50, "Карие") {}
+    Mother(string name, int age)
+    {
+        this->name = name;
+        this->age = age;
+    }
+    virtual void Show()
+    {
+        Human::Show();
+        cout << "\t цвет глаз: " << eyes << endl;
+    }
+    void SetEyes(string value)
+    {
+        eyes = value;
+    }
+    string GetEyes()
+    {
+        return eyes;
+    }
+    virtual int GetGender()
+    {
+        return gender;
+    }
+};
+class Father : public Human
+{
+protected:
+    string hair;
+
+public:
+    Father(string name, int age, int height, int weight, string hair) : hair{ hair }
+    {
+        this->name = name;
+        this->age = age;
+        this->height = height;
+        this->weight = weight;
+        this->gender = 'M';
+        this->hair = hair;
+    }
+    Father() {}
+    Father(int height, int weight)
+    {
+        this->height = height;
+        this->weight = weight;
+        this->gender = 'M';
+    }
+    virtual void Show()
+    {
+        Human::Show();
+
+        cout << "\t цвет волос: " << hair << endl;
+    }
+    void SetHair(string value)
+    {
+        hair = value;
+    }
+    string GetHair()
+    {
+        return hair;
+    }
+};
+class Student : public Mother, public Father
+{
+protected:
+    int ID;
+public:
+    Student(string name, int age, int height, int weight, string hair, string eyes, int ID) : ID{ ID }
+    {
+        Mother::name = name;
+        Mother::age = age;
+        Father::height = height;
+        Father::weight = weight;
+        Father::gender = 'M';
+        this->hair = hair;
+        this->eyes = eyes;
+    }
+    Student() : Student("Пузо", 17, 180, 80, "Чёрный", "Карие", 182207) {}
+    int GetID()
+    {
+        return ID;
+    }
+    void SetID(int value)
+    {
+        ID = value;
+    }
+    virtual void Show()
+    {
+        //Father::Show();
+        //Mother::Show();
+        cout << Mother::name << "\t возраст: " << Mother::age << "\t рост:" << Father::height << "\t вес: " << Father::weight << "\t цвет глаз: " << eyes << "\t цвет волос: " << hair << "\t Id: " << ID << endl;
+    }
+};
 
 int main()
 {
@@ -130,6 +289,7 @@ int main()
     cat.show();
     cat.say();
     //Pet pet;
+    cout << "___________________________________________________________________________" << endl;
     
     //Animal beast;
     Frog frog;
@@ -138,6 +298,18 @@ int main()
     frog.Say();
     giraffe.Show();
     giraffe.Say();
+    cout << "___________________________________________________________________________" << endl;
+
+    Mother M("Mama", 29, 190, 50, "Зелёные");
+    Father P("Papa", 30, 240, 200, "Блондин");
+    Student S("Chypep", 12, 180, 60, "Блондин", "Зелёные", 182207);
+
+    M.Show();
+    cout << "___________________________________________________________________________" << endl;
+    P.Show();
+    cout << "___________________________________________________________________________" << endl;
+    S.Show();
+    cout << char(S.Father::GetGender()) << endl;
 
     return 0;
 }
