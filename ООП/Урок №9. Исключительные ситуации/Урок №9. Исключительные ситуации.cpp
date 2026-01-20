e28998f416e43499d5bb58f38c8d0d03a073c073
Paul Winter <zimin.pavel.v@gmail.com>
2026-01-20 18:21:00 +0300

    обработка деления на ноль

--- ООП/Урок №9. Исключительные ситуации/Урок №9. Исключительные ситуации.cpp
+++ ООП/Урок №9. Исключительные ситуации/Урок №9. Исключительные ситуации.cpp
@@ -7,60 +7,34 @@ using namespace std;
 // throw (бросать)
 // catch (поймать)
 
-int Div(int x, int y)
-{
-    // попытка деления на ноль вызовет код ошибки: 1010
-    if (y == 0)
-    {
-        throw y;
-    }
-    return x / y;
-}
-
-void Division(int x, int y)
-{
-    if (y == 0)
-    {
-        cout << "На ноль делить нельзя!" << endl;
-    }
-    else
-    {
-        cout << "x / y = " << x / y << endl;
-    }
-}
-
-int SomeFunc()
-{
-    //try
-    //{
-        // блок кода, в котором может возникнуть исключительная ситуация
-        //...
-        //throw выражение определённого типа
-    //}
-    //catch (тип_исключения имя)
-    //{
-    //}
-    return 0;
-}
-
 int main()
 {
     setlocale(LC_ALL, "");
 
-    //cout << "6 / 0 = " << Div(6, 0) << endl;
-    //Division(6, 0);
+    while (true)
+    {
+        try
+        {
+            double a;
+            double b;
 
-    try
-    {
-        cout << "Перед ошибкой" << endl;
-        throw 100;
-        cout << "После ошибки" << endl;
+            cout << "\nВведите делимое: ";
+            cin >> a;
+            cout << "\nВведите делитель: ";
+            cin >> b;
+
+            if (b == 0)
+            {
+                throw b;
+            }
+
+            cout << "\nРезультат деления: " << a / b << endl;
+        }
+        catch (double ex)
+        {
+            cout << "\nОшибка - попытка деления на " << ex << endl;
+        }
     }
-    catch(int ex)
-    {
-        cout << "Возникла ошибка с кодом: " << ex << endl;
-    }
-    cout << "Программа продолжила своё выполнение" << endl;
 
     return 0;
 }
