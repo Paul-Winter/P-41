bb4b9ffe29a6a55944447a559fc135e47e885cb5
Paul Winter <zimin.pavel.v@gmail.com>
2026-02-03 19:24:03 +0300

    работа с классом list

--- ООП/Урок №13. Контейнеры/Урок №13. Контейнеры.cpp
+++ ООП/Урок №13. Контейнеры/Урок №13. Контейнеры.cpp
@@ -1,7 +1,27 @@
 ﻿#include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
+
+void ShowLists(list<int>& l1, list<int>& l2)
+{
+    // создаём итератор
+    list<int>::iterator iter;
+
+    cout << "list1: ";
+    for (iter = l1.begin(); iter != l1.end(); iter++)
+    {
+        cout << *iter << " ";
+    }
+    cout << "\n____________________________________________________________" << endl;
+    cout << "list2: ";
+    for (iter = l2.begin(); iter != l2.end(); iter++)
+    {
+        cout << *iter << " ";
+    }
+    cout << "\n____________________________________________________________" << endl;
+}
 
 int main()
 {
@@ -10,7 +30,7 @@ int main()
 #pragma region vector
 
     cout << "==============================vector==============================" << endl;
-    // создаём вектор
+    // создаём объект динамического массива
     vector<int> vec;
 
     cout << "Ёмкость вектора: " << vec.capacity() << endl;
@@ -66,9 +86,48 @@ int main()
 
 #pragma region list
 
-    //cout << endl << endl;
-    //cout << "==============================list==============================" << endl;
+    cout << endl << endl;
+    cout << "==============================list==============================" << endl;
+    // создаём объекты двунаправленного связного списка
+    list<int> list1;
+    list<int> list2;
 
+    cout << "Заполняем списки значениями..." << endl;
+    for (int i = 0; i < 6; i++)
+    {
+        list1.push_back(i);
+        list2.push_front(i);
+    }
+    cout << "____________________________________________________________" << endl;
+    cout << "Выводим списки:" << endl;
+    ShowLists(list1, list2);
+    cout << "В первом списке перемещаем первый элемент в конец..." << endl;
+    list1.splice(list1.end(),   // позиция в приёмнике
+                 list1,         // источник
+                 list1.begin());// позиция в источнике
+    ShowLists(list1, list2);
+    cout << "Во втором списке перемещаем первый элемент в конец..." << endl;
+    list2.splice(list2.end(),   // позиция в приёмнике
+                 list2,         // источник
+                 list2.begin());// позиция в источнике
+    ShowLists(list1, list2);
+    cout << "Разворачиваем первый список..." << endl;
+    list1.reverse();
+    cout << "____________________________________________________________" << endl;
+    ShowLists(list1, list2);
+    cout << "Сортируем оба списка..." << endl;
+    list1.sort();
+    list2.sort();
+    cout << "____________________________________________________________" << endl;
+    ShowLists(list1, list2);
+    cout << "Сливаем два списка в первый..." << endl;
+    list1.merge(list2);
+    cout << "____________________________________________________________" << endl;
+    ShowLists(list1, list2);
+    cout << "Удаляем дубликаты из первого списка..." << endl;
+    list1.unique();
+    cout << "____________________________________________________________" << endl;
+    ShowLists(list1, list2);
 
 #pragma endregion
 
