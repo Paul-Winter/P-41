ad8b801c85122b53acf861f6d744fcd82ad9d0a6
Paul-Winter <zimin.pavel.v@gmail.com>
2026-03-17 18:42:19 +0300

    диаграммы классов. отношения

---
+++ UML/ClassDiagram/ClassDiagram.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Position
+{
+private:
+    double MinimalSalary;
+    string Name;
+
+public:
+    double GetMinSalary()
+    {
+        return MinimalSalary;
+    }
+    string GetName()
+    {
+        return Name;
+    }
+    Position(string _name, double _minimalSalary) : Name {_name}, MinimalSalary {_minimalSalary}
+    {        
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    cout << "Привет, Мир!" << endl;
+
+    return 0;
+}
