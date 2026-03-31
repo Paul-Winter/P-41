82175945097adb434613a0d82516bd8d2fa5af4e
Paul Winter <zimin.pavel.v@gmail.com>
2026-03-31 19:47:49 +0300

    диаграмма классов - отношения и паттерн одиночка

---
+++ Паттерны проектирования/Singleton/Singleton.cpp
@@ -0,0 +1,53 @@
+﻿// Singleton.cpp//
+
+#include <iostream>
+
+using namespace std;
+
+class Singleton
+{
+private:
+    static Singleton* instance;
+    string name;
+    Singleton()
+    {
+
+    }
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+public:
+    string GetName()
+    {
+        return name;
+    }
+    void SetName(string _name)
+    {
+        this->name = _name;
+    }
+    static Singleton* GetInstance()
+    {
+        if (instance == nullptr)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+};
+Singleton* Singleton::instance = nullptr;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    Singleton* first = Singleton::GetInstance();
+    first->SetName("одиночка");
+    cout << "имя объекта №1: " << first->GetName() << endl;
+
+    Singleton* second = Singleton::GetInstance();
+    cout << "имя объекта №2: " << second->GetName() << endl;
+
+    Singleton* third = Singleton::GetInstance();
+    cout << "имя объекта №3: " << third->GetName() << endl;
+
+    return 0;
+}
---
+++ Паттерны проектирования/Диаграмма классов/Диаграмма классов.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Organ
+{
+private:
+    string name;
+public:
+    string GetName()
+    {
+        return name;
+    }
+    void SetName(string _name)
+    {
+        this->name = _name;
+    }
+};
+
+class Human
+{
+private:
+    string name;
+    Organ* heart;
+public:
+    string GetName()
+    {
+        return name;
+    }
+    void SetName(string _name)
+    {
+        this->name = _name;
+    }
+    Human()
+    {
+        this->heart = new Organ;
+    }
+};
+
+class IWork
+{
+public:
+    virtual void Work() = 0;
+};
+
+class Employee : public Human, public IWork
+{
+private:
+    double salary;
+public:
+    double GetSalary()
+    {
+        return salary;
+    }
+    void SetSalary(double _salary)
+    {
+        this->salary = _salary;
+    }
+    virtual void Work() override
+    {
+        cout << "работник работает" << endl;
+    }
+};
+
+class Workplace
+{
+private:
+    int number;
+    Employee worker;
+public:
+    int GetNumber()
+    {
+        return number;
+    }
+    void SetNumber(int _number)
+    {
+        this->number = _number;
+    }
+    Workplace(Employee employee)
+    {
+        this->worker = employee;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    cout << "Привет, Мир!" << endl;
+
+    return 0;
+}
