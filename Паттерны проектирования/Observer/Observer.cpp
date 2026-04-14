e1ebc57f5caac6ddd252af3ee471399ed16d232c
Paul Winter <zimin.pavel.v@gmail.com>
2026-04-14 19:30:07 +0300

    добавлены паттерны strategy и observer

---
+++ Паттерны проектирования/Observer/Observer.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class IObserver
+{
+public:
+    virtual void Update() = 0;
+};
+class ConcreteObserver : public IObserver
+{
+public:
+    ConcreteObserver()
+    {
+        cout << "создан наблюдатель" << endl;
+    }
+    void Update() override
+    {
+        cout << "наблюдатель изменился" << endl;
+    }
+};
+class SMS : public IObserver
+{
+public:
+    SMS()
+    {
+    }
+    void Update() override
+    {
+        cout << "отправлено SMS-сообщение" << endl;
+    }
+};
+class Notification : public IObserver
+{
+public:
+    Notification()
+    {
+    }
+    void Update() override
+    {
+        cout << "отправлено PUSH-оповещение" << endl;
+    }
+};
+class EmailMessage : public IObserver
+{
+public:
+    EmailMessage()
+    {
+    }
+    void Update() override
+    {
+        cout << "отправлено письмо на электронную почту" << endl;
+    }
+};
+class IObservable
+{
+public:
+    virtual void AddObserver(IObserver* obs) = 0;
+    virtual void RemoveObserver(IObserver* obs) = 0;
+    virtual void NotifyObservers() = 0;
+};
+class ConcreteObservable : public IObservable
+{
+private:
+    vector<IObserver*> observers;
+public:
+    void AddObserver(IObserver* obs) override
+    {
+        cout << "наблюдатель подписался на объект наблюдения" << endl;
+        observers.push_back(obs);
+    }
+    void RemoveObserver(IObserver* obs) override
+    {
+        cout << "наблюдатель удалён из списка наблюдателей" << endl;
+        observers.erase(remove(observers.begin(), observers.end(), obs), observers.end());
+    }
+    void NotifyObservers() override
+    {
+        for (IObserver* observer : observers)
+        {
+            cout << "объект наблюдения оповещает наблюдателя" << endl;
+            observer->Update();
+        }
+    }
+    ConcreteObservable()
+    {
+        cout << "создан объект наблюдения" << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    
+    ConcreteObservable* person = new ConcreteObservable();
+    SMS* sms = new SMS();
+    Notification* push = new Notification();
+    EmailMessage* email = new EmailMessage();
+
+    person->AddObserver(sms);
+    person->AddObserver(push);
+    person->AddObserver(email);
+    person->NotifyObservers();
+
+    person->RemoveObserver(push);
+    person->NotifyObservers();
+
+    return 0;
+}
---
+++ Паттерны проектирования/Strategy/Strategy.cpp
@@ -0,0 +1,129 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class IStrategy
+{
+public:
+    virtual void Operation() = 0;
+};
+class ConcreteStrategy1 : public IStrategy
+{
+public:
+    void Operation() override
+    {
+        cout << "Concrete Strategy #1" << endl;
+    }
+};
+class ConcreteStrategy2 : public IStrategy
+{
+public:
+    void Operation() override
+    {
+        cout << "Concrete Strategy #2" << endl;
+    }
+};
+class Context
+{
+private:
+    IStrategy* strategy;
+public:
+    void SetStrategy(IStrategy* strategy)
+    {
+        this->strategy = strategy;
+    }
+    void ExecuteOperation()
+    {
+        strategy->Operation();
+    }
+};
+class IShooting
+{
+public:
+    virtual void Shooting() = 0;
+};
+class Pistol : public IShooting
+{
+private:
+    string name;
+public:
+    string GetName()
+    {
+        return name;
+    }
+    void Shooting() override
+    {
+        cout << "стреляет из пистолета " << name << endl;
+    }
+    Pistol(string name)
+    {
+        this->name = name;
+    }
+};
+class ShotGun : public IShooting
+{
+private:
+    string name;
+public:
+    string GetName()
+    {
+        return name;
+    }
+    void Shooting() override
+    {
+        cout << "стреляет из автомата " << name << endl;
+    }
+    ShotGun(string name)
+    {
+        this->name = name;
+    }
+};
+class Player
+{
+private:
+    IShooting* shooting;
+public:
+    void SetGun(IShooting* shooting)
+    {
+        this->shooting = shooting;
+    }
+    void Shooting()
+    {
+        shooting->Shooting();
+    }
+    Player()
+    {
+        cout << "создан персонаж игрока" << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    Context context;
+    IStrategy* cs1 = new ConcreteStrategy1();
+    IStrategy* cs2 = new ConcreteStrategy2();
+
+    context.SetStrategy(cs1);
+    context.ExecuteOperation();
+    
+    context.SetStrategy(cs2);
+    context.ExecuteOperation();
+
+    cout << "____________________________________________________________________________" << endl;
+
+    Player* player = new Player();
+    IShooting* pistol = new Pistol("Глок");
+    IShooting* shotGun = new ShotGun("АК-74");
+
+    cout << "игрок подбирает новое оружие" << endl;
+    player->SetGun(pistol);
+    player->Shooting();
+
+    cout << "игрок подбирает новое оружие" << endl;
+    player->SetGun(shotGun);
+    player->Shooting();
+
+    return 0;
+}
