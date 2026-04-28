db92ed252571cac3260efcad932cddb092a6f426
Paul Winter <zimin.pavel.v@gmail.com>
2026-04-28 17:36:03 +0300

    первые проекты на C#

---
+++ C#/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static int Main()
+        {
+            Console.Write("Привет! Как тебя зовут? ");
+            String name = Console.ReadLine();
+            const string abc = "dlskfjsdfjasdklfjs";
+            int def = 14;
+            Console.Clear();
+            Console.Write($"Hello {name}. Укажите свой год рождения: ");
+            int birthyear = Int32.Parse(Console.ReadLine());
+            Console.Clear();
+            int age = 2026 - birthyear;
+
+            if (age > def)
+            {
+                Console.WriteLine($"Йоу, {name}! Тебе уже {age} лет");
+            }
+            else if (age == def)
+            {
+                Console.WriteLine("Тебе 14 лет! Пора получать паспорт!");
+            }
+            else
+            {
+                Console.WriteLine("Тю, малолетка!");
+            }
+            return 0;
+        }
+    }
+}
---
+++ C#/hello_world/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Привет! Как тебя зовут? ");
+string name = Console.ReadLine();
+const string abc = "dlskfjsdfjasdklfjs";
+int def = 14;
+Console.Clear();
+Console.Write($"Hello {name}. Укажите свой год рождения: ");
+int birthyear = Int32.Parse(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Йоу, {name}! Тебе уже {2026 - birthyear} лет");
