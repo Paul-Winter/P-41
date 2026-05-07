dd1b64a031d1ba707e9b75e8ed694beaf280a551
Paul Winter <zimin.pavel.v@gmail.com>
2026-05-07 18:08:07 +0300

    перегрузка операторов

---
+++ C#/Урок №4. Перегрузка/Point.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Урок__4._Перегрузка
+{
+    public class Point
+    {
+        private int x;
+        private int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Point(int a): this(a, a) {}
+        public Point(): this(0, 0) {}
+        public void Show()
+        {
+            Console.WriteLine($"x = {x}; y = {y};");
+        }
+        //public static тип_возвращаемого_значения operator символ_операции(параметры) {}
+        public static Point operator -(Point a)
+        {
+            return new Point(-a.x, -a.y);
+        }
+        public static Point operator -(Point a, Point b)
+        {
+            return new Point(a.x - b.x, a.y - b.y);
+        }
+        public static Point operator +(Point a, Point b)
+        {
+            return new Point(a.x + b.x, a.y + b.y);
+        }
+        public static bool operator ==(Point a, Point b)
+        {
+            return a.x == b.x && a.y == b.y;
+        }
+        public static bool operator !=(Point a, Point b)
+        {
+            return !(a == b);
+        }
+        public static bool operator true(Point a)
+        {
+            return a.x != 0 || a.y != 0 ? true : false;
+        }
+        public static bool operator false(Point a)
+        {
+            return a.x == 0 && a.y == 0 ? true : false;
+        }
+        public override string ToString()
+        {
+            return $"Point ({x};{y})";
+        }
+    }
+}
---
+++ C#/Урок №4. Перегрузка/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Урок__4._Перегрузка
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 3);
+            Point p2 = new Point(2);
+            Point p3 = new Point();
+
+            p1.Show();
+            p2.Show();
+            p3.Show();
+
+            Point pResult = -p2;
+            pResult.Show();
+
+            pResult = p1 + p1;
+            pResult.Show();
+            pResult = p2 - p1;
+            pResult.Show();
+            if (p1)
+            {
+                Console.WriteLine($"p1 is true");
+                Console.WriteLine(p1);
+            }
+            if (p2)
+            {
+                Console.WriteLine($"p2 is true");
+                Console.WriteLine(p2);
+            }
+            if (p3)
+            {
+                Console.WriteLine(p3);
+            }
+            else
+            {
+                Console.WriteLine($"p3 is false");
+                Console.WriteLine(p3);
+            }
+
+        }
+    }
+}
