c9816ce9c41a1377253701466c46f94ede1a4863
Paul Winter <zimin.pavel.v@gmail.com>
2026-05-12 19:41:55 +0300

    домашняя работа

---
+++ C#/ДЗ_Машинки/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ДЗ_Машинки
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Vehicle[] vehicles = new Vehicle[] {
+                new Car(),
+                new Tractor(),
+                new Motocycle() };
+            foreach (Vehicle vehicle in vehicles)
+            {
+                Console.WriteLine(vehicle);
+            }
+        }
+    }
+}
---
+++ C#/ДЗ_Машинки/Vehicle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ДЗ_Машинки
+{
+    internal abstract class Vehicle
+    {
+        protected string name;
+        protected int maxspeed;
+        public override string ToString()
+        {
+            return $"{name}; {maxspeed};";
+        }
+        protected Vehicle(string name, int maxspeed)
+        {
+            this.maxspeed = maxspeed;
+            this.name = name;
+        }
+        protected Vehicle() : this("unknown", 0) {}
+    }
+    internal class Car : Vehicle
+    {
+        public int passagers;
+        public override string ToString()
+        {
+            return $"{name}; {maxspeed}; {passagers};";
+        }
+        public Car() : base("Обычная машина", 100)
+        {
+            passagers = 4;
+        }
+    }
+    internal class Tractor : Vehicle
+    {
+        public int maxWeight;
+        public override string ToString()
+        {
+            return $"{name}; {maxspeed}; {maxWeight};";
+        }
+        public Tractor() : base("Обычный трактор", 30)
+        {
+           maxWeight = 15000;
+        }
+    }
+    internal class Motocycle : Vehicle
+    {
+        public bool ifSidecar;
+        public override string ToString()
+        {
+            return $"{name}; {maxspeed}; {ifSidecar};";
+        }
+        public Motocycle() : base("Обычный мотоцикл", 150)
+        {
+            ifSidecar = false;
+        }
+    }
+}
