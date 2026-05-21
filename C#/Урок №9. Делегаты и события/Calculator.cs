ca1682f88be17fa173ef9ff7c84505f3f6b903e2
Paul Winter <zimin.pavel.v@gmail.com>
2026-05-21 19:53:10 +0300

    лямбда-выражения

--- C#/Урок №9. Делегаты и события/Calculator.cs
+++ C#/Урок №9. Делегаты и события/Calculator.cs
@@ -7,25 +7,10 @@ namespace Урок__9._Делегаты_и_события
     public delegate double CalcDelegate(double a, double b);
     public class Calculator
     {
-        public double Sum(double a, double b)
-        {
-            return a + b;
-        }
-        public double Sub(double a, double b)
-        {
-            return a - b;
-        }
-        public double Mul(double a, double b)
-        {
-            return a * b;
-        }
-        public double Div(double a, double b)
-        {
-            if (b != 0)
-            {
-                return a / b;
-            }
-            throw new DivideByZeroException();
-        }
+        // лямбда-выражения
+        public double Sum(double a, double b) => a + b;
+        public double Sub(double a, double b) => a - b;
+        public double Mul(double a, double b) => a * b;
+        public double Div(double a, double b) => b != 0 ? a / b : throw new DivideByZeroException();
     }
 }
