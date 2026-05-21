e72ababf38a53a26ddf6f93f81d0f8d05ec56f2f
Paul Winter <zimin.pavel.v@gmail.com>
2026-05-21 18:41:29 +0300

    делегаты

---
+++ C#/Урок №9. Делегаты и события/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Урок__9._Делегаты_и_события
+{
+    public delegate double CalcDelegate(double a, double b);
+    public class Calculator
+    {
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+        public double Div(double a, double b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            throw new DivideByZeroException();
+        }
+    }
+}
---
+++ C#/Урок №9. Делегаты и события/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Урок__9._Делегаты_и_события
+{
+    public delegate int IntDelegate(double d);
+    public class Program
+    {
+        //class MyDelegate : MulticastDelegate
+        //{
+
+        //}
+
+        // модификатор_доступа delegate тип_данных ИмяДелегата(параметры);
+
+        delegate void VoidDelegate(int i);
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Calculator calculator = new Calculator();
+                Console.Write("Введите выражение (например: X+Y): ");
+                string expression = Console.ReadLine();
+                char operation = ' ';
+                if (String.IsNullOrEmpty(expression))
+                {
+                    return;
+                }
+                foreach (char item in expression)
+                {
+                    if (item == '+' || item == '-' || item == '*' || item == '/')
+                    {
+                        operation = item;
+                        break;
+                    }
+                }
+                //Console.WriteLine($"Вы ввели {operation} операцию");
+                try
+                {
+                    string[] numbers = expression.Split(operation);
+                    double a = Double.Parse(numbers[0]);
+                    double b = Double.Parse(numbers[1]);
+                    CalcDelegate calcDel = null;
+                    switch (operation)
+                    {
+                        case '+': calcDel = new CalcDelegate(calculator.Sum); break;
+                        case '-': calcDel = new CalcDelegate(calculator.Sub); break;
+                        case '*': calcDel = new CalcDelegate(calculator.Mul); break;
+                        case '/': calcDel = new CalcDelegate(calculator.Div); break;
+                        default: throw new InvalidOperationException();
+                    }
+                    Console.WriteLine($"Результат: {calcDel(a, b)}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+            }
+        }
+    }
+}
