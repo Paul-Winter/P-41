053f9aff88de48391e18141ec9d5d0b45adadab1
Paul Winter <zimin.pavel.v@gmail.com>
2026-05-26 18:43:04 +0300

    необобщённые коллекции

---
+++ C#/Урок №10. Введение в Generics/Program.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+
+namespace Урок__10._Введение_в_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("_______________________Массив_______________________");
+            char[] hello = new char[] { 'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!' };
+            //string hello = "Hello, World!";
+            Console.WriteLine(hello);
+            for (int i = 0; i < hello.Length; i++)
+            {
+                Console.WriteLine(hello[i]);
+            }
+
+            Console.WriteLine("=======================Необобщённые_коллекции:=======================");
+
+            Console.WriteLine("_______________________Динамический_массив_______________________");
+            ArrayList arrList1 = new ArrayList();
+            ArrayList arrList2 = new ArrayList(5);
+            ArrayList arrList3 = new ArrayList(new int[] { 1, 4, 28, 7, 5 });
+            Console.WriteLine($"Вместимость по умолчанию arrList1: {arrList1.Capacity}");
+            arrList1.Add("one");
+            Console.WriteLine($"Вместимость после добавления элемента в arrList1: {arrList1.Capacity}");
+            arrList1.AddRange(new int[] { 2, 8, 1, 4 });
+            Console.WriteLine($"Вместимость после добавления коллекции в arrList1: {arrList1.Capacity}");
+            arrList1.Capacity = 10;
+            Console.WriteLine($"Вместимость установлена в arrList1: {arrList1.Capacity}");
+            Console.WriteLine($"Фактическое количество элементов в arrList1: {arrList1.Count}");
+            arrList1.TrimToSize();
+            Console.WriteLine($"Вместимость в arrList1 уменьшена до количества элементов: {arrList1.Capacity}");
+            Console.WriteLine($"__________Все элементы коллекции:");
+            foreach (object item in arrList1)
+            {
+                Console.WriteLine($"\t{item}");
+            }
+            Console.WriteLine($"__________Вставка нового элемента:");
+            arrList1.Insert(2, "hello");
+            foreach (object item in arrList1)
+            {
+                Console.WriteLine($"\t{item}");
+            }
+            Console.WriteLine($"__________Индекс элемента: {arrList1.IndexOf("hello")}");
+            Console.WriteLine($"__________Удаление элемента:");
+            arrList1.RemoveAt(2);
+            foreach (object item in arrList1)
+            {
+                Console.WriteLine($"\t{item}");
+            }
+
+            ArrayList days = new ArrayList(new string[] {
+                "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"});
+            Console.WriteLine("__________Неделя:");
+            foreach (object day in days)
+            {
+                Console.WriteLine($"День недели {day}");
+            }
+            ArrayList workWeek = new ArrayList(days.GetRange(0, 5));
+            Console.WriteLine("__________Рабочая неделя:");
+            foreach (object day in workWeek)
+            {
+                Console.WriteLine($"Рабочий день {day}");
+            }
+            ArrayList weekEnd = new ArrayList(days.GetRange(5, 2));
+            Console.WriteLine("__________Выходные:");
+            foreach (object day in weekEnd)
+            {
+                Console.WriteLine($"Выходной день {day}");
+            }
+            Console.WriteLine("_______________________Стек_______________________");
+            Stack stack1 = new Stack();
+            Stack stack2 = new Stack(7);
+            Stack stack3 = new Stack(new ArrayList { 3, 8, 5 });
+            Console.WriteLine("__________Push:");
+            stack1.Push("one");
+            stack1.Push("two");
+            stack1.Push("three");
+            foreach (object item in stack1)
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.WriteLine($"__________Pop: {stack1.Pop()}");
+            foreach (object item in stack1)
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.WriteLine($"__________Peek: {stack1.Peek()}");
+            foreach (object item in stack1)
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.WriteLine("_______________________Очередь_______________________");
+            Queue q1 = new Queue();
+            Queue q2 = new Queue(5);
+            Queue q3 = new Queue(new ArrayList { "one", 4, 8.3 });
+            Queue q4 = new Queue(7, 3.0f);
+            foreach (object item in q3)
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.WriteLine($"__________Enqueue:");
+            q3.Enqueue("two");
+            foreach (object item in q3)
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.WriteLine($"__________Dequeue: {q3.Dequeue()}");
+            Console.WriteLine($"__________Dequeue: {q3.Dequeue()}");
+            foreach (object item in q3)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+            Console.WriteLine("_______________________Хеш-таблица_______________________");
+            Hashtable hash = new Hashtable();
+            hash.Add(1, "John");
+            hash.Add("two", 22222);
+            foreach (object item in hash.Keys)
+            {
+                Console.WriteLine($"Ключ {item} : Значение {hash[item]}");
+            }
+            foreach (object item in hash.Values)
+            {
+                Console.WriteLine($"значение {item}");
+            }
+            Console.WriteLine("_______________________Сортированный_список_______________________");
+            SortedList sL = new SortedList();
+            sL.Add(3, 6.7);
+            sL.Add(2, "two");
+            sL.Add(1, "one");
+            foreach (object item in sL.Keys)
+            {
+                Console.WriteLine($"Ключ {item} - значение {sL[item]}");
+            }
+            Console.WriteLine("\n\n");
+            //Console.WriteLine("=======================Обобщённые_коллекции:=======================");
+        }
+    }
+}
