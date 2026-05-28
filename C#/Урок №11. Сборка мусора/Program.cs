a2ed9096538c002af1a03cb54cdd2c4867d723b9
Paul Winter <zimin.pavel.v@gmail.com>
2026-05-28 19:10:29 +0300

    работа финализатора и IDisposable

--- C#/Урок №11. Сборка мусора/Program.cs
+++ C#/Урок №11. Сборка мусора/Program.cs
@@ -12,6 +12,7 @@
             Example ex = new Example();
             Console.WriteLine($"ex.a = {ex.a}");
             Console.WriteLine($"ex.b = {ex.b}");
+            ex.Dispose();
 
             Console.WriteLine("_________________________Демонстрация работы System.GC_________________________");
             Console.WriteLine($"Максимальное поколение: {GC.MaxGeneration}");
@@ -29,9 +30,26 @@
             GC.Collect();
             Console.WriteLine($"Занято памяти: {GC.GetTotalMemory(false)} байт");
             Console.WriteLine($"Поколение объекта: {GC.GetGeneration(helper)}");
+            Console.WriteLine("_________________________Демонстрация работы финализатора и IDisposable_________________________");
+
+            DisposeExample test1 = new DisposeExample();
+            try
+            {
+                test1.DoSomething();
+            }
+            finally
+            {
+                test1.Dispose();
+            }
+
+            using (DisposeExample test2 = new DisposeExample())
+            {
+                test2.DoSomething();
+                test2.Dispose();
+            }
         }
     }
-    public class GarbageHelper
+    public class GarbageHelper : IDisposable
     {
         public class Person
         {
@@ -46,10 +64,59 @@
                 Person p = new Person();
             }
         }
+
+        public void Dispose()
+        {
+            Console.WriteLine("!!!Очистка ресурсов!!!");
+        }
+        //protected override void Finalize() { }
+        //~GarbageHelper()
+        //{
+
+        //}
     }
-    public class Example
+    public class Example : IDisposable
     {
         public int a;
         public int b;
+
+        public void Dispose()
+        {
+            Console.WriteLine("!!!Очистка ресурсов!!!");
+        }
+    }
+
+    public class DisposeExample : IDisposable
+    {
+        private bool isDisposed = false;
+        private void Cleaning(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    Console.WriteLine("Освобождаются управляемые ресурсы");
+                }
+                Console.WriteLine("Освобождаются неуправляемые ресурсы");
+            }
+            isDisposed = true;
+        }
+        public void Dispose()
+        {
+            Cleaning(true);
+            GC.SuppressFinalize(this);
+        }
+        public void DoSomething()
+        {
+            Console.WriteLine("Выполнение определённых операций");
+        }
+        public DisposeExample()
+        {
+            Console.WriteLine("Выделение памяти...\nИнициализация...");
+        }
+        ~DisposeExample()
+        {
+            Cleaning(false);
+        }
     }
 }
