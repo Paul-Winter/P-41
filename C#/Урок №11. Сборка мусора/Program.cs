90039a3a03f51c964e4c366a5c5831b9b34b4876
Paul Winter <zimin.pavel.v@gmail.com>
2026-05-28 18:15:39 +0300

    работа сборщика мусора

---
+++ C#/Урок №11. Сборка мусора/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Урок__11._Сборка_мусора
+{
+    //  Garbage Collector
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = new int();
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            Example ex = new Example();
+            Console.WriteLine($"ex.a = {ex.a}");
+            Console.WriteLine($"ex.b = {ex.b}");
+
+            Console.WriteLine("_________________________Демонстрация работы System.GC_________________________");
+            Console.WriteLine($"Максимальное поколение: {GC.MaxGeneration}");
+            GarbageHelper helper = new GarbageHelper();
+            Console.WriteLine($"Поколение объекта: {GC.GetGeneration(helper)}");
+            Console.WriteLine($"Занято памяти: {GC.GetTotalMemory(false)} байт");
+            Console.WriteLine("Создаём мусор...");
+            helper.MakeGarbage();
+            Console.WriteLine($"Занято памяти: {GC.GetTotalMemory(false)} байт");
+            Console.WriteLine("Вызываем сборщик мусора в 0 поколении");
+            GC.Collect(0);
+            Console.WriteLine($"Занято памяти: {GC.GetTotalMemory(false)} байт");
+            Console.WriteLine($"Поколение объекта: {GC.GetGeneration(helper)}");
+            Console.WriteLine("Вызываем сборщик мусора во всех поколениях");
+            GC.Collect();
+            Console.WriteLine($"Занято памяти: {GC.GetTotalMemory(false)} байт");
+            Console.WriteLine($"Поколение объекта: {GC.GetGeneration(helper)}");
+        }
+    }
+    public class GarbageHelper
+    {
+        public class Person
+        {
+            string name;
+            string surname;
+            int age;
+        }
+        public void MakeGarbage()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Person p = new Person();
+            }
+        }
+    }
+    public class Example
+    {
+        public int a;
+        public int b;
+    }
+}
