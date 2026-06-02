c22255d368e8ca4d8e0ad12729fa9c62ce598b9c
Paul Winter <zimin.pavel.v@gmail.com>
2026-06-02 17:25:01 +0300

    изменены темы экзаменационных проектов

---
+++ C#/Урок №12. Файловая система/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Урок__12._Файловая_система
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
