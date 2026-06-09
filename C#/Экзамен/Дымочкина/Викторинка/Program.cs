fccee9b69a093ef56547e8307b2ca3d1537a773c
Anastasiya Dymochkina <nastyadymoc@mail.ru>
2026-06-09 19:26:18 +0300

    создан экзаменационный проект

---
+++ C#/Экзамен/Дымочкина/Викторинка/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Викторинка
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
