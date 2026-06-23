31792d3a18f50cf1c866fbc5fcc64ac57fb42161
Paul Winter <zimin.pavel.v@gmail.com>
2026-06-23 18:55:23 +0300

    Введение в Windows Forms

---
+++ Windows Forms/Урок №1. First App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Урок__1.First_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form1 = new Form();
+            form1.Text = "First Windows Forms Application";
+            form1.ShowDialog();
+        }
+    }
+}
---
+++ Windows Forms/Урок №1. Введение в Windows Forms/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Урок__1.Введение_в_Windows_Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Вы нажали на кнопку!",
+                                                  "Это - диалоговое окно",
+                                                  MessageBoxButtons.YesNoCancel,
+                                                  MessageBoxIcon.Exclamation);
+            if (result == DialogResult.Yes)
+            {
+                MessageBox.Show("Вы нажали кнопку 'YES'");
+            }
+            else if (result == DialogResult.No)
+            {
+                MessageBox.Show("Вы нажали кнопку 'NO'");
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                this.Close();
+            }
+        }
+    }
+}
